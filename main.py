5a9863b96572cfa654ea531fa2af5d9faae4a097
Alejandro <aandradez094@gmail.com>
2026-07-12 21:47:08 -0500

    Preparación de routers y main base

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from routers import (
+    productos,
+    clientes,
+    usuarios,
+    ventas,
+    inventario,
+    devoluciones,
+    proveedores
+)
+
+app = FastAPI(
+    title="API ANCISA",
+    description="Sistema REST para la gestión comercial de ANCISA S.A.",
+    version="1.0.0"
+)
 
 @app.get("/")
-def read_root():
-    return {"message": "¡Hola, Fast API!"}
+def root():
+    return {
+        "message": "API ANCISA funcionando correctamente"
+    }
+
+app.include_router(productos.router)
+app.include_router(clientes.router)
+app.include_router(usuarios.router)
+app.include_router(ventas.router)
+app.include_router(inventario.router)
+app.include_router(devoluciones.router)
+app.include_router(proveedores.router)
---
+++ routers/usuarios.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/usuarios",
+    tags=["Usuarios"]
+)
